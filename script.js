79bd67114b98413153acadf80d927bdde194f374
hvillatoro20 <hypatiavillatoro@yahoo.com.mx>
2024-12-26 01:55:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("whatsapp-hato").addEventListener("click", function () {
+    window.open("https://wa.me/50487655578", "_blank");
+});
+
+document.getElementById("whatsapp-minitas").addEventListener("click", function () {
+    window.open("https://wa.me/50432131376", "_blank");
+});
+
+document.getElementById("facebook").addEventListener("click", function () {
+    window.open("https://facebook.com/plussaludhn", "_blank");
+});
+
+document.getElementById("instagram").addEventListener("click", function () {
+    window.open("https://instagram.com/plussaludhn", "_blank");
+});
+
+document.getElementById("gmail").addEventListener("click", function () {
+    window.open("mailto:plusalud.info@gmail.com", "_blank");
+});
